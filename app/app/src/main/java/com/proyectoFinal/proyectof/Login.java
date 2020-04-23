4ebc6d78ff553b2aa4c4b08c56f722467aa2136b
thecalored <calored11@gmail.com>
2020-04-23 18:58:08 +0200

    comit
    layout login terminado
    layout registrarse creado

--- app/app/src/main/java/com/proyectoFinal/proyectof/Login.java
+++ app/app/src/main/java/com/proyectoFinal/proyectof/Login.java
@@ -2,6 +2,7 @@ package com.proyectoFinal.proyectof;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-    TextView text_titulo,text_usuario,text_pass;
+    TextView text_titulo,text_usuario,text_pass,click_registrarse;
     EditText input_usuario,input_pass;
     Button b_inicio;
     @Override
@@ -27,6 +28,14 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Login.this,"hola que tal",Toast.LENGTH_SHORT).show();
+            }
+        });
+        click_registrarse = (TextView) findViewById(R.id.click_registrarse);
+        click_registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pagRegistrarse = new Intent(Login.this,Registrarse.class);
+                startActivity(pagRegistrarse);
             }
         });
     }
