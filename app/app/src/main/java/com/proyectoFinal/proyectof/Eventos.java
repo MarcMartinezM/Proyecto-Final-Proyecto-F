bd08f26dc626c0f65eeaf8ce6cc4d3249ec96141
thecalored <calored11@gmail.com>
2020-04-24 21:42:12 +0200

    commit
    commit dia 24/4

--- app/app/src/main/java/com/proyectoFinal/proyectof/Eventos.java
+++ app/app/src/main/java/com/proyectoFinal/proyectof/Eventos.java
@@ -1,6 +1,18 @@
 package com.proyectoFinal.proyectof;
 
-public class Eventos {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 
+public class Eventos extends AppCompatActivity {
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
