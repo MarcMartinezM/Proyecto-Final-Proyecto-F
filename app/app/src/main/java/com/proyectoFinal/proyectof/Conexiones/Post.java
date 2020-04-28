e593396a207975e187a3f87a0d245b4f7e49df34
AdrianSalas98 <adriansalase@gmail.com>
2020-04-28 18:30:06 +0200

    Commit

---
+++ app/app/src/main/java/com/proyectoFinal/proyectof/Conexiones/Post.java
@@ -0,0 +1,15 @@
+package com.proyectoFinal.proyectof.Conexiones;
+
+import com.proyectoFinal.proyectof.Objectos.Usuario;
+
+import java.net.HttpURLConnection;
+
+public class Post {
+    Usuario u1 = new Usuario("Adrián", "Salas", "Asalas1", "123321", "adsa1@gmail.com", 87654, 600908765);
+
+    public void post(String completeUrl, String body) {
+
+    }
+
+
+}
--- app/app/src/main/java/com/proyectoFinal/proyectof/Objectos/Evento.java
+++ app/app/src/main/java/com/proyectoFinal/proyectof/Objectos/Evento.java
@@ -9,7 +9,6 @@ public class Evento {
     String foto_evento;
 
     public Evento() {
-        super();
 
     }
 
