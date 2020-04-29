7df3932ee7c198804cb6d54d59442bafb77bd8fe
AdrianSalas98 <adriansalase@gmail.com>
2020-04-29 18:30:31 +0200

    commit
    post para testing

--- app/app/src/main/java/com/proyectoFinal/proyectof/Conexiones/Post.java
+++ app/app/src/main/java/com/proyectoFinal/proyectof/Conexiones/Post.java
@@ -2,13 +2,25 @@ package com.proyectoFinal.proyectof.Conexiones;
 
 import com.proyectoFinal.proyectof.Objectos.Usuario;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Post {
     Usuario u1 = new Usuario("Adrián", "Salas", "Asalas1", "123321", "adsa1@gmail.com", 87654, 600908765);
@@ -27,8 +39,31 @@ public class Post {
         }
     }
 
-    public void post(HttpURLConnection conn, URL url) {
+    private static String encodeParams(JSONObject params) throws Exception {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        Iterator<String> itr = params.keys();
+        while(itr.hasNext()){
+            String key= itr.next();
+            Object value = params.get(key);
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+        }
+        return result.toString();
+    }
+
+    public void post(HttpURLConnection conn, URL url) throws Exception {
         try {
+            JSONObject obj = new JSONObject();
+            obj.put("email","roberwido@gmail.com");
+            obj.put("password","scarlet123321");
+
             url = new URL("http://proyectof.tk/api/user/login");
             conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(20000);
@@ -38,13 +73,33 @@ public class Post {
             conn.setDoOutput(true);
             InputStream in = new BufferedInputStream(conn.getInputStream());
             readStream(in);
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(os, "UTF-8"));
+            writer.write(encodeParams(obj));
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode=conn.getResponseCode();
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader br=new BufferedReader( new InputStreamReader(conn.getInputStream()));
+                StringBuffer sb = new StringBuffer("");
+                String line="";
+                while((line = br.readLine()) != null) {
+                    sb.append(line);
+                    break;
+                }
+                in.close();
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         } finally {
             conn.disconnect();
         }
     }
-
 }
